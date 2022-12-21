206d3bb96e3d0892bb3f82f4b6589d469d7aa8b5
donnyh0730 <donnyh0730@gmail.com>
2022-12-21 16:24:28 +0900

    Changed Config file location and modified ConfigManager.

--- Server/Server/Data/ConfigManager.cs
+++ Server/Server/Data/ConfigManager.cs
@@ -19,7 +19,7 @@ namespace Server.Data
 
         public static void LoadConfig()
         {
-            string text = File.ReadAllText("config.json");
+            string text = File.ReadAllText("../../../../../Common/config.json");
             Config = Newtonsoft.Json.JsonConvert.DeserializeObject<ServerConfig>(text);
         }
     }
